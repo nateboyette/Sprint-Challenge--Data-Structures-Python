5f0a970797627ce43906e1a558304c40b6c05bb9
Nate Boyette <nateboyette@icloud.com>
2019-05-10 09:18:07 -0700

    Completed ring buffer

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -5,7 +5,45 @@ class RingBuffer:
     self.storage = [None]*capacity
 
   def append(self, item):
-    pass
+    # if current index is less than the capacity 
+    # change the value at the index to the item
+    if self.current < self.capacity:
+      self.storage[self.current] = item
+      self.current += 1
+    else:
+      # if the index reaches the capacity
+      # reset to 0 and replace the first
+      # item in the list
+      self.current = 0
+      self.storage[self.current] = item
+      self.current += 1
 
   def get(self):
-    pass
+    newList = []
+    # create new list of items without
+    # the possibility of any None values
+    for i in range(len(self.storage)):
+      if self.storage[i] is not None:
+        newList.append(self.storage[i])
+    return newList
+
+
+
+
+
+newBuff = RingBuffer(5)
+
+newBuff.append("a")
+newBuff.append("b")
+newBuff.append("c")
+newBuff.append("d")
+newBuff.get()
+newBuff.append("e")
+newBuff.get()
+newBuff.append('f')
+newBuff.get()
+newBuff.append('g')
+newBuff.append('h')
+newBuff.append('i')
+newBuff.get()
+
