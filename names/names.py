1d432994682d97e2987137844613cc69d9689406
Nate Boyette <nateboyette@icloud.com>
2019-05-10 12:34:11 -0700

    Completed names.py using sets. Couldn't figure out how to do it with other data structures

--- names/names.py
+++ names/names.py
@@ -1,11 +1,8 @@
 import time
-from binary_search_tree import BinarySearchTree
-from max_heap import Heap
+
 
 start_time = time.time()
 
-
-nameList = BinarySearchTree("Sample Name")
 
 f = open('names_1.txt', 'r')
 names_1 = f.read().split("\n")  # List containing 10000 names
@@ -16,19 +13,21 @@ names_2 = f.read().split("\n")  # List containing 10000 names
 
 f.close()
 
-duplicates = Heap()
+duplicates = []
+
+nameList1 = set()
+nameList2 = set()
 
 for name in names_1:
-    duplicates.insert(name)
-    print(name)
+    nameList1.add(name)
 
 for name in names_2:
-    duplicates.insert(name)
+    nameList2.add(name)
 
-print(duplicates)
+duplicates = nameList1.intersection(nameList2)
 
 end_time = time.time()
-# print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
+print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
 print (f"runtime: {end_time - start_time} seconds")
 
 
